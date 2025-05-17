40fdd9ec225ed7cf8fbf411c12cf3ae18ce3ad17
dvkdesigns <dvkdesigns@gmail.com>
2025-05-17 15:58:14 -0400

    Updating homepage redirect

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        has: [
+          {
+            type: 'host',
+            value: 'parklandguitarlessons.com',
+          },
+        ],
+        destination: 'https://www.parklandguitarlessons.com/:1',
+        permanent: true,
+      },
+    ];
   },
 };
 
