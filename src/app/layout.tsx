bfa0210b4fb00045438cf11b5415b3338fe03beb
dvkdesigns <dvkdesigns@gmail.com>
2025-05-28 12:57:34 -0400

    GA Code update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   {process.env.NODE_ENV === 'production' && (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id="
+        src="https://www.googletagmanager.com/gtag/js?id=G-W7MB5Q62LC"
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -42,7 +42,7 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '');
+          gtag('config', 'G-W7MB5Q62LC');
         `}
         
       </Script>
